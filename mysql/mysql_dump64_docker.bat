721b50689e193a458511361c1176efee0c615d12
luxiaochen <494985971@qq.com>
2020-02-15 23:40:07 +0800

    dev

--- mysql/mysql_dump64_docker.bat
+++ mysql/mysql_dump64_docker.bat
@@ -1,4 +1,4 @@
 @ECHO off
 set wdb=hzwfdb_manager_rds
-docker run -i --network network1 --rm mysql:5.7 mysqldump -hmysql -uroot -p123456 --ignore-table=hzwfdb_manager_rds.* %wdb% >"hzwfdb_manager_rds.sql"
+docker run -i --network docker-dev_network1 --rm mysql:5.7 mysqldump -hmysql -uroot -p123456 --ignore-table=hzwfdb_manager_rds.* %wdb% >"hzwfdb_manager_rds.sql"
 pause
