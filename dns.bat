2b08caa5fdf5cf012c9a61d0b7b9f7e7bfdcdd48
luxiaochen <494985971@qq.com>
2020-02-22 21:38:23 +0800

    dev

--- dns.bat
+++ dns.bat
@@ -2,8 +2,8 @@
 set/p ip=192.168.1.
 set trueip=192.168.1.%ip%
 
-aliyun alidns UpdateDomainRecord --RecordId 19123530803843072 --RP hzwf-manager --Type A --Value %trueip% --Line default
-aliyun alidns UpdateDomainRecord --RecordId 19123601903715328 --RP hzwf-wx --Type A --Value %trueip% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19123530803843072 --RR hzwf-manager --Type A --Value %trueip% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19123601903715328 --RR hzwf-wx --Type A --Value %trueip% --Line default
 
 ::aliyun alidns DescribeSubDomainRecords  --SubDomain hzwf-manager.luxiaochen.com
 
