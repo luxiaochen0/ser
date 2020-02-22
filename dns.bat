885d53ba9abfd4aadcb6de29fc56afad21208667
luxiaochen <494985971@qq.com>
2020-02-22 23:37:15 +0800

    dev

--- dns.bat
+++ dns.bat
@@ -1,9 +1,12 @@
 @ECHO off
-set/p ip=192.168.1.
-set trueip=192.168.1.%ip%
+set/p ipv4=192.168.1.
+set trueipv4=192.168.1.%ip%
+set/p ipv6=input ipv6
 
-aliyun alidns UpdateDomainRecord --RecordId 19123530803843072 --RR hzwf-manager --Type A --Value %trueip% --Line default
-aliyun alidns UpdateDomainRecord --RecordId 19123601903715328 --RR hzwf-wx --Type A --Value %trueip% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19123530803843072 --RR hzwf-manager --Type A --Value %trueipv4% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19238100006797312 --RR hzwf-manager --Type AAAA --Value %ipv6% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19123601903715328 --RR hzwf-wx --Type A --Value %trueipv4% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19238100867903488 --RR hzwf-wx --Type AAAA --Value %ipv6% --Line default
 
 ::aliyun alidns DescribeSubDomainRecords  --SubDomain hzwf-manager.luxiaochen.com
 
