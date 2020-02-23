3d3c734e522a02c789cd38935a62171921d2fb86
luxiaochen <494985971@qq.com>
2020-02-23 20:08:36 +0800

    dns脚本添加域名

--- dns.bat
+++ dns.bat
@@ -1,12 +1,14 @@
 @ECHO off
 set/p ipv4=192.168.1.
 set trueipv4=192.168.1.%ipv4%
-set/p ipv6=input ipv6
+::set/p ipv6=input ipv6
 
 aliyun alidns UpdateDomainRecord --RecordId 19123530803843072 --RR hzwf-manager --Type A --Value %trueipv4% --Line default
-aliyun alidns UpdateDomainRecord --RecordId 19238100006797312 --RR hzwf-manager --Type AAAA --Value %ipv6% --Line default
+::aliyun alidns UpdateDomainRecord --RecordId 19238100006797312 --RR hzwf-manager --Type AAAA --Value %ipv6% --Line default
 aliyun alidns UpdateDomainRecord --RecordId 19123601903715328 --RR hzwf-wx --Type A --Value %trueipv4% --Line default
-aliyun alidns UpdateDomainRecord --RecordId 19238100867903488 --RR hzwf-wx --Type AAAA --Value %ipv6% --Line default
+::aliyun alidns UpdateDomainRecord --RecordId 19238100867903488 --RR hzwf-wx --Type AAAA --Value %ipv6% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19196542328179712 --RR next-superhero --Type A --Value %trueipv4% --Line default
+aliyun alidns UpdateDomainRecord --RecordId 19196990950567936 --RR next-superhero-api --Type A --Value %trueipv4% --Line default
 
 ::aliyun alidns DescribeSubDomainRecords  --SubDomain hzwf-manager.luxiaochen.com
 
