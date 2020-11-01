5d017ec554c3ce40e118f69f194256dc100de8ff
luxiaochen <494985971@qq.com>
2020-11-02 00:06:26 +0800

    修改一些内容

--- mysql/dump.bat
+++ mysql/dump.bat
@@ -1,3 +1,3 @@
 @ECHO off
-docker run --network docker-dev_network1 --rm mysql:5.7.30 mysqldump -hmysql5.7 -uroot -p123456 --databases hzwf_manager hzwf_express hzwf_fashion  >"hzwf.sql"
+docker run --network ser_network1 --rm mysql:5.7.30 mysqldump -hmysql5.7 -uroot -p123456 --databases hzwf_manager hzwf_express hzwf_fashion  >"hzwf.sql"
 pause
