a19a13f9a6fca270b11b211dc310d1162e3ca241
luxiaochen <494985971@qq.com>
2020-12-29 17:39:41 +0800

    更新软件

--- mysql/dump.bat
+++ mysql/dump.bat
@@ -1,3 +1,3 @@
 @ECHO off
-docker run --network ser_network1 --rm mysql:5.7.30 mysqldump -hmysql5.7 -uroot -p123456 --databases hzwf_manager hzwf_express hzwf_fashion hzwf_mijue >"hzwf.sql"
+docker run --network ser_network1 --rm mysql:5.7.32 mysqldump -hmysql5.7 -uroot -p123456 --databases hzwf_manager hzwf_express hzwf_fashion hzwf_mijue >"hzwf.sql"
 pause
--- mysql/import.sh
+++ mysql/import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i --network ser_network1 --rm mysql:5.7.30 mysql -hmysql5.7 -uroot -p123456 <"hzwf.sql"
+docker run -i --network ser_network1 --rm mysql:5.7.32 mysql -hmysql5.7 -uroot -p123456 <"hzwf.sql"
